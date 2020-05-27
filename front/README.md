531c47355f193ae4f61893fc955bbdd0bd8ab70f
jakubfilistynski <j.filistynski@bonasoft.pl>
2020-05-27 18:43:53 +0200

    podpiecie routera

--- front/README.md
+++ front/README.md
@@ -1,4 +1,4 @@
-# code
+# front
 
 ## Project setup
 ```
--- front/src/main.js
+++ front/src/main.js
@@ -1,44 +1,10 @@
-import Vue from 'vue'
-// import App from './App.vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-// Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueRouter)
-
-const Foo = { component: {
-    template: "<div>Foo</div>"
-  } }
-const Bar = { component: {
-    template: "<div>Bar</div>"
-  } }
-import Home from "./components/HelloWorld"
-
-const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar },
-  { path: '/', component: Home}
-]
-
-const router = new VueRouter({
-  routes // short for `routes: routes`
-})
-
-// render: h => h(App),
 new Vue({
   router,
-  template: `
-    <div>
-      <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item"><router-link to="/" class="nav-link">Home</router-link></li>
-            <li class="nav-item"><router-link to="/about" class="nav-link">About</router-link></li>
-            <li class="nav-item"><router-link to="/contact" class="nav-link">Contact</router-link></li>
-          </ul>
-        </div>
-      </nav>
-      <router-view class="view"></router-view>
-    </div>
-  `
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
