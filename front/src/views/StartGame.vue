581f7924a4574a1e648c7019c46cd99dbfa0be57
jakubfilistynski <j.filistynski@bonasoft.pl>
2020-05-27 23:29:11 +0200

    wylogowanie

--- front/src/views/StartGame.vue
+++ front/src/views/StartGame.vue
@@ -11,9 +11,14 @@
             reloaded: false
         }),
         mounted() {
-            if (!this.reloaded) {
-                this.reloaded = true;
-                location.reload()
+            if (localStorage.getItem('reloaded')) {
+                // The page was just reloaded. Clear the value from local storage
+                // so that it will reload the next time this page is visited.
+                localStorage.removeItem('reloaded');
+            } else {
+                // Set a flag so that we know not to reload the page twice.
+                localStorage.setItem('reloaded', '1');
+                location.reload();
             }
         },
         methods: {},
