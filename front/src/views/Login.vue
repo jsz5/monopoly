9249fd2eb87bc6104f95f93d26bce6b2e1d0a75c
jakubfilistynski <j.filistynski@bonasoft.pl>
2020-05-27 21:16:36 +0200

    WIP: logowanie + Board.vue

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-<!--    <HelloWorld msg="Welcome to Your Vue.js App" />-->
-
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <v-text-field
+        v-model="form.username"
+        :error-messages="errors.login"
+        label="Login"
+        required
+    ></v-text-field>
+    <v-text-field
+        v-model="form.password"
+        :error-messages="errors.password"
+        label="E-mail"
+        required
+    ></v-text-field>
+    <v-btn class="mr-4" @click="submit">submit</v-btn>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+    // @ is an alias to /src
+    // import HelloWorld from "@/components/HelloWorld.vue";
+    import axios from 'axios'
+    import baseUrl from "../config";
 
-export default {
-  name: "Login",
-  // components: {
-  //   HelloWorld
-  // }
-};
+    export default {
+        name: "Login",
+        data: () => ({
+            form: {
+                username: '',
+                password: '',
+            },
+            errors: {
+                login: '',
+                password: '',
+            },
+            url: baseUrl + '/api/login/'
+        }),
+        methods: {
+            submit() {
+                console.log(this.form.login)
+                console.log(this.form.password)
+                axios.post(this.url, this.form)
+                    .then(response => {
+                      console.log(response);
+                    })
+                    .catch(error => {
+                      console.log(error);
+                    })
+            },
+        },
+        // components: {
+        //   HelloWorld
+        // }
+    };
 </script>
