22aabeaeedc6c3bc3607a1a5adb2a7986d790093
artantica <karo.antonik@gmail.com>
2020-06-01 13:29:27 +0200

    Skonfigurowanie websocketow - routing.

--- back/api/routing.py
+++ back/api/routing.py
@@ -4,7 +4,6 @@ from . import consumers
 
 websockets = URLRouter([
     re_path(r'ws/lobby/$', consumers.LobbyConsumer, name='lobby'),
-    re_path(r'ws/lobby$', consumers.LobbyConsumer, name='lobby'),
-    re_path(r'ws/game/$', consumers.BoardConsumer)
+    re_path(r'ws/game/$', consumers.BoardConsumer, name='board')
     # re_path(r'ws/board/(?P<board_name>\w+)/$', consumers.BoardConsumer),
 ])
